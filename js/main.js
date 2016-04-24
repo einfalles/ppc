42c6697603319516f71bba6a7b94b582a2e32f98
einfalles <dn5jg@virginia.edu>
2016-04-23 17:48:47 -0700

    yes

--- js/main.js
+++ js/main.js
@@ -2,12 +2,17 @@ $(document).ready(function(){
   $w = $(window)
   $w.on('resize',res);
   res();
-});
 
-// HELPER FUNCTIONS
-function res() {
-  console.log($w.outerHeight());
-  var a = $('.hero');
-  var b = $('header');
-  a.css('height',($w.outerHeight()-b.outerHeight())+"px");
-}
+
+
+  // HELPER FUNCTIONS
+  function res() {
+    console.log("window height: "+$w.outerHeight());
+    var a = $('.hero');
+    var b = $('header');
+    console.log("header height: "+b.outerHeight());
+    a.css('height',($w.outerHeight()-b.outerHeight())+"px");
+    console.log("hero height: "+a.outerHeight());
+  }
+
+});
