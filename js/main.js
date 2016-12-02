3d1c957b846eef1f7dea1f6dc9098d569c181c5a
Duylam <dlamnguyen@gmail.com>
2016-12-01 21:33:31 -0800

    wah

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  a = location.href;
+  console.log(a);
   $("#demo01").animatedModal();
   $w = $(window)
   $w.on('resize',res);
@@ -17,6 +19,22 @@ $(document).ready(function(){
     a.css('height',($w.outerHeight()-b.outerHeight())+"px");
     console.log("hero height: "+a.outerHeight());
   }
+
+    var d = new Date();
+    var hour = d.getHours();
+    var day = d.getDay();
+    switch(true){
+      case (hour < 11):
+        $("#menu-select").val("breakfast").change();
+        break;
+      case (hour < 20 && hour > 11):
+        $("#menu-select").val("dinner").change();
+        break;
+      case (hour > 20):
+
+        $("#menu-select").val("drinks").change();
+        break;
+    }
   $('#menu-select').change(function(){
     console.log(this.value);
     if (this.value == 'breakfast') {
